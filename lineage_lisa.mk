d3253e2d22257f48928575b43f755f5835dd6645
ComradeWave <tomfoxy0852@protonmail.com>
2024-07-27 14:22:42 +0200

    lisa: Adapt for pixelstar 2
    Forgot to change to Pixelstar directory when searching for the common_full_phone  confg

--- lineage_lisa.mk
+++ lineage_lisa.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelstar stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
